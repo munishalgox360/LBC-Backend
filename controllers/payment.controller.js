cd16f1c3eeed09e65a79a3ec311acc5d2bf7b62a
surajalgox360 <suraj@algox360.com>
2024-03-21 12:43:54 +0530

    transaction listing

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -36,18 +36,25 @@ const VerifyPayment = async (req, res) => {
     try {
         const genSign = crypto.createHmac('sha256', process.env.RAZORPAY_SECRET).update(keys.toString()).digest('hex');
         if(genSign === razorpay_signature){
+            const detail = await PAYMENT.payments.fetch(razorpay_payment_id);
+
             const createPayload = {
                 userId : userId,
-                razorpay_order_id : razorpay_order_id, 
-                razorpay_payment_id : razorpay_payment_id,
-                razorpay_signature : razorpay_signature
+                amount : detail.amount,
+                currencyUnit : detail.currency,
+                method : detail.method,
+                status : detail.status,
+                paymentId : razorpay_payment_id,
+                orderId : razorpay_order_id
             }
+
             const createResp = await PaymentModel.create(createPayload);
             if(createResp){
-                const resp = await PaymentInvoiceSES(createResp);
-                if(resp) return res.redirect('https://algox360.com/');
+                // const resp = await PaymentInvoiceSES(createResp);
+                // if(resp)
+                 return res.status(200).json({ status : 201, message : "Valid Transaction" });
             }else{
-                return res.status(200).json({ status : 401, response : createResp, message : message.create_f });
+                return res.status(200).json({ status : 401, message : "Invalid Transaction" });
             }
         }
     } catch (error) {
@@ -57,13 +64,27 @@ const VerifyPayment = async (req, res) => {
 
 
 const ReadTransaction = async (req, res) => {
-    
+
+    let getResp;
     const userId = new ObjectId(req.userId);
-    const skip = Number(req.query.skip);
-    
+    const page = Number(req.query.page);
+    const filter = { $and: [ 
+                            {userId : userId}, 
+                            {$and : [ 
+                                {createdAt : { $gte : new Date(req.query.startDate) }}, 
+                                {createdAt :{ $lte : new Date(req.query.endDate) }}
+                            ]} 
+                        ] 
+                    };
+                    
     try {
-        const getResp = await PaymentModel.find({ userId : userId }).skip((skip - 1)*20).limit(20);
-        if(getResp){
+        if(req.query.startDate && req.query.endDate && userId){
+            getResp = await PaymentModel.find(filter).skip((page - 1)*20).limit(20);
+        }else{
+            getResp = await PaymentModel.find({ userId : userId }).skip((page - 1)*20).limit(20);
+        }
+        
+        if(getResp.length > 0){
             return res.status(200).json({ status : 201, response : getResp, message : message.read_s, length : getResp.length });
         }else{
             return res.status(200).json({ status : 401, response : getResp, message : message.read_f });
@@ -74,21 +95,7 @@ const ReadTransaction = async (req, res) => {
 };
 
 
-const ReadPayment = async (req, res) => {
-    const paymentId = req.query.paymentId;
-    try {
-        const getResp = await PAYMENT.payments.fetch({ id : paymentId }).promise();
-        if(getResp){
-            return res.status(200).json({ status : 201, response : getResp, message : message.read_s });
-        }else{
-            return res.status(200).json({ status : 401, response : getResp, message : message.read_f });
-        }
-    } catch (error) {
-        res.status(400).json({ status : 400, response : error.stack, message : error.message });
-    }
-};
-
-export { CreateOrder, VerifyPayment, ReadTransaction, ReadPayment };
+export { CreateOrder, VerifyPayment, ReadTransaction };
 
 
 
