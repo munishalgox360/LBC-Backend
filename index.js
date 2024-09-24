18fc2ca9ada4a0dc9c21bbaf40c1f3494e0fb427
surajalgox360 <suraj@algox360.com>
2024-09-24 16:12:03 +0530

    certificates

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan'
 import cors from 'cors';
+import https from "https";
 import connectDB  from './config/dbconnect.js';
 import router from './routes/index.route.js';
+
+
+// Load SSL certificates
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/fullchain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/chain.pem', 'utf8'); 
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
 
 // Create App
 const app = express();
 const PORT = process.env.PORT || 8000;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
@@ -35,4 +47,7 @@ app.get("/",function(req, res){
 app.use('/api', router);
 
 
-app.listen(PORT,console.log(`Express listening on port ${PORT}`));
+// Create HTTPS server
+https.createServer(credentials, app).listen(PORT, () => {
+    console.log(`Express HTTPS server listening on port ${PORT}`);
+});
